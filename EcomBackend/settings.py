e9a07849e88336ccf5bf84a706ec11491c86aca7
shujaakbar2020 <shujaakbar2020@gmail.com>
2022-10-05 13:14:35 +0500

    update for heroku deployment

--- EcomBackend/settings.py
+++ EcomBackend/settings.py
@@ -82,17 +82,23 @@ WSGI_APPLICATION = 'EcomBackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ecombackend',
+#         'USER': 'ecombackend',
+#         'PASSWORD': 'ecombackend123',
+#         'HOST': '127.0.0.1',
+#         'PORT': 5432
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ecombackend',
-        'USER': 'ecombackend',
-        'PASSWORD': 'ecombackend123',
-        'HOST': '127.0.0.1',
-        'PORT': 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn shop-by-shuja.wsgi
